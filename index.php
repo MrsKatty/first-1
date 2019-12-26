2138c31a1603dbadcf0737a3d10f86d6af2acfe6
zhaneuski <59090092+zhaneuski@users.noreply.github.com>
2019-12-26 21:17:16 +0300

    объекты

--- index.php
+++ index.php
@@ -10,87 +10,224 @@
 
 <body>
     <?php
-    echo "Hello World!";
 
 
-    $a = 5;
-    $b = 1;
-    $c = $a << $b;
-
-    echo "<br>";
-    echo $c;
-
-    echo "<br>";
-
-    $bool = true;
-    $str = "foo";
-    $int = 12;
-
-    echo gettype($bool);
-
-    echo "<br>";
-
-    echo gettype($str);
-
-    echo "<br>";
-
-    echo gettype($int);
+    // echo "Hello World!";
 
 
-    //Если это целое,  увеличить на четыре
+    // $a = 5;
+    // $b = 1;
+    // $c = $a << $b;
 
-    if (is_int($int)) {
-        $int += 4;
-    }
+    // echo "<br>";
+    // echo $c;
 
+    // echo "<br>";
 
-    // если $bool это  строка то вывести ее
-    // (ничего не выводит)
+    // $bool = true;
+    // $str = "foo";
+    // $int = 12;
 
-    if (is_string($bool)) {
+    // echo gettype($bool);
 
-        echo "Строка : $bool";
-    }
+    // echo "<br>";
 
-    echo "<br>";
+    // echo gettype($str);
 
+    // echo "<br>";
 
-    $a = 1234;
-
-    echo $a;
-
-    echo "<br>";
-
-    $a = -1234;
-    echo $a;
-
-    echo "<br>";
-
-    $a = 1.2e3;
-
-    echo $a;
-    echo "<br>";
-
-    $b = 7E-10;
-    echo $b;
+    // echo gettype($int);
 
 
-    echo "<br>";
-    echo 'Чтобы вывести \' нужно поставить \\';
+    // //Если это целое,  увеличить на четыре
 
-    echo "<br>";
-    echo 'это не вставит: \n новую строку';
+    // if (is_int($int)) {
+    //     $int += 4;
+    // }
 
-    echo "<br>";
 
-    $a = 3;
-    echo "\$a=$a";
+    // // если $bool это  строка то вывести ее
+    // // (ничего не выводит)
 
-    echo "<br>";
+    // if (is_string($bool)) {
 
-    echo "ковычка \" \n ковычка ";
+    //     echo "Строка : $bool";
+    // }
+
+    // echo "<br>";
+
+
+    // $a = 1234;
+
+    // echo $a;
+
+    // echo "<br>";
+
+    // $a = -1234;
+    // echo $a;
+
+    // echo "<br>";
+
+    // $a = 1.2e3;
+
+    // echo $a;
+    // echo "<br>";
+
+    // $b = 7E-10;
+    // echo $b;
+
+
+    // echo "<br>";
+    // echo 'Чтобы вывести \' нужно поставить \\';
+
+    // echo "<br>";
+    // echo 'это не вставит: \n новую строку';
+
+    // echo "<br>";
+
+    // $a = 3;
+    // echo "\$a=$a";
+
+    // echo "<br>";
+
+    // echo "ковычка \" \n ковычка ";
+
+    // echo "<br>";
+
+    // echo "это не вставит <br>\n новую строку";
+
+    // echo "<br>";
+
+
+
+
+    //=================================================================
+
+
+
+
+    // М А С С И В Ы
+
+
+
+    // $arr = array("foo" => "bar", 12 => true);
+    // echo $arr["foo"];
+
+    // echo "<br>";
+
+    // echo $arr[12];
+
+
+    // $arr = array("somearray" => array(6 => 5, 13 => 9, "a" => 42));
+
+    // echo $arr["somearray"][6];
+
+    // echo "<br>";
+
+    // echo $arr["somearray"][13];
+
+    // echo "<br>";
+
+    // echo $arr["somearray"]["a"];
+
+
+
+
+
+
+    // $arr = array(5 => 43, 46, 48, "b" => 12);
+    //     echo $arr[5];
+    //     echo "<br>";
+    //     echo $arr[6];
+    //     echo "<br>";
+    //     echo $arr[7];
+    //     echo "<br>";
+    //     echo $arr["b"];
+    //     echo "<br>";
+
+    //     echo "<pre>";
+    //     print_r($arr);
+    //     echo "</pre>";
+
+
+
+    // $arr = array( 5=> 1, 12 => 2);
+    // print_r($arr);
+    // echo "<br>";
+
+    // $arr[] = 56;
+
+    // print_r($arr);
+
+    // echo "<br>";
+
+    // $arr["x"] = 48;
+    // print_r($arr);
+
+    // echo "<br>";
+
+    // unset($arr[5]); //--удаляет элемент из массива.
+
+    // print_r($arr);
+
+    // echo "<br>";
+    // unset($arr); //--удаляет весь массив.
+    // print_r($arr);
+
+
+
+
+    //ОБЪЕКТЫ ===================================================
+
+    // class foo
+    // {
+    //     function do_foo()
+    //     {
+    //         echo "doing foo";
+    //     }
+    // }
+
+    // $bar = new foo;
+    // $bar ->do_foo();
+
+
+
+
+    // Оператор if (Условия) ===================================================
+
+    // $a = 12;
+    // $b = 15;
+
+    // if ($a < $b)
+    //     echo "Выражение верно";
+
+    //     echo "<br>";
+
+    // if ($a < $b) {
+    //     echo "Выражение верно";
+    //     $a = $b;
+    // }
+
+    // echo "<br>";
+
+    // echo $a;
+
+    // echo "<br>";
+
+    // if ($a < $b) {
+    //     echo "Выражение верно";
+
+    //     echo "<br>";
+    // }
+
+    // else {
+    //     echo "Выражение ложно";
+    // }
+
+    // echo "<br>";
 
     ?>
+
 </body>
 
 </html>
