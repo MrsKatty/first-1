a1ab55cce84cabc19cf70b33404b21239784fbd6
zhaneuski <59090092+zhaneuski@users.noreply.github.com>
2020-02-24 23:23:40 +0300

    new

--- 19fev/file-sharing/rename.php
+++ 19fev/file-sharing/rename.php
@@ -1,7 +1,10 @@
 <?php
 
 $old_name = file_get_contents("rename.txt");
-$new_name = $_POST["rename"];
+$arr_old_name = explode(".", $old_name);
+
+$arr_new_name = [$_POST["rename"], $arr_old_name[1]];
+$new_name = implode(".", $arr_new_name);
 
 rename("file/$old_name", "file/$new_name");
 
