c48b7c32b6447ccde9d16c768eb92b3dda377bc0
zhaneuski <59090092+zhaneuski@users.noreply.github.com>
2020-03-01 17:35:51 +0300

    cz

--- 19fev/file-sharing/download.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (is_uploaded_file($_FILES['userfile']['tmp_name'])) {
-copy($_FILES['userfile']['tmp_name'], "file\\" . $_FILES['userfile']['name']);
-}
-
-header("Location: show.php");
--- 19fev/file-sharing/new_text.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-file_put_contents("file/$_POST[old_name]", "$_POST[area]");
-
-header("Location: show.php");
-
-?>
