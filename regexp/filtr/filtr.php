7ff0b433ae485e06674f483d124433b8ab84d076
zhaneuski <59090092+zhaneuski@users.noreply.github.com>
2020-03-03 21:18:19 +0300

    weew

--- regexp/filtr/filtr.php
+++ regexp/filtr/filtr.php
@@ -1,6 +1,12 @@
 <?php
+$arr_words = file('words.txt');
 
-if (preg_match("/редиска|питон/ui", $_POST['filtr'])) {
+foreach ($arr_words as &$word) {
+   $word = trim($word);
+}
+$words = implode('|',$arr_words);
+
+if (preg_match("/$words/ui", $_POST['filtr'])) {
     echo "Не ругайся!";
 }else {
     echo "Молодец!";
