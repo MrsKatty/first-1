42333c816904684f6d0e15de8fc71402f342490b
zhaneuski <59090092+zhaneuski@users.noreply.github.com>
2020-05-12 21:35:23 +0300

    113

--- --OOP--/tic-tac-toe/test2.php
+++ --OOP--/tic-tac-toe/test2.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,61 +8,53 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        .tictac td {
-            border: 1px solid black;
-            width: 100px;
-            height: 100px;
-            text-align: center;
-
-        }
-
-        a {
-            display: block;
-            width: 100%;
-            height: 100%;
-        }
-
-        img {
-            display: block;
-            position: relative;
-            top: 25px;
-            left: 25px;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
+    <a href="?action=newgame">новая игра</a>
     <?php
     include 'autoload.php';
 
 
-    $ai = new Ai();
+    $ai = new Ai(3);
 
-    $ai->initMap(3)
-       ->putCross(1, 1);
+    //$ai->setSaver(new JsonFile('map.json'));
+    //$ai->setSaver(new PHPFile('map.php'));
+    $ai->setSaver(new SessionSaver('map'));
 
-    // $area = new Area();
+    $ai->loadMap();
 
-    // $area->setMap($ai->getMap());
 
-    // echo $area->html();
+    switch ($_GET['action']) {
+        case 'shot':
+            if (!empty($ai->searchEmptyCells()) && $ai->checWin() === null) {
+                $ai->putCross($_GET['i'], $_GET['j']);
+            }
+            $ai->putRandNull();
+            break;
+        case 'newgame':
+            $ai->initMap(3);
+            break;
+    }
 
-    // $ai->selfGaming();
-    // if ($ai->checWin() === 1) {
-    //     echo "Выйграли крестики!";
-    // } elseif ($ai->checWin() === 0) {
-    //     echo "Выйграли нолики!";
-    // } else {
-    //     echo "Ничья!";
-    // }
+    $ai->saveMap();
+ 
+    if (empty($ai->searchEmptyCells()) || $ai->checWin() !== null) {
+        if ($ai->checWin() === 1) {
+            echo "Выйграли крестики!";
+        } elseif ($ai->checWin() === 0) {
+            echo "Выйграли нолики!";
+        } else {
+            echo "Ничья!";
+        }
+    }
 
     $area = new Area();
 
     $area->setMap($ai->getMap());
 
     echo $area->html();
-
 
     ?>
 </body>
